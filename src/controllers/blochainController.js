e1106cca7341058be1f1e2056484d00592e14e89
Gyaneshwer <gyanshwarmishra1234@gmail.com>
2022-12-07 12:54:19 +0530

    Initial Commit

---
+++ src/controllers/blochainController.js
@@ -0,0 +1,7 @@
+const axios = require("axios")
+
+let getAssets = async function(req,res){
+          try{
+                    let 
+          }
+}
---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const routes = require("./routes/route.js");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json);
+
+mongoose
+  .connect(
+    "mongodb+srv://Gyaneshwer694:gYaN0694Mdb@cluster1.i15rwas.mongodb.net/?retryWrites=true&w=majority",
+    { UseNewUrlParser: true }
+  )
+
+  .then(() => console.log("DB is Connected"))
+  .catch((err) => console.log(err.message));
+
+app.use("/", routes);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("listening at " + (process.env.PORT || 3000));
+});
